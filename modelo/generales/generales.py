07ca06fb84235b0f887b846a44c64cf258e2e4f7
Jean Trujillo <00zacks00@gmail.com>
2025-02-07 19:57:52 -0500

    refactor: se usan variables de entorno para evitar exponer keys

--- modelo/generales/generales.py
+++ modelo/generales/generales.py
@@ -1,7 +1,9 @@
-from modelo.supabase.keys import *
+# from modelo.supabase.keys import *
 from librerias import *
+from dotenv import load_dotenv
+import os
+load_dotenv()   
 
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
-
+supabase = create_client(os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_KEY'))
 
 tabla_usuarios = 'TABLA_USUARIOS'
