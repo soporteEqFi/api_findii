23e7bdc902d1a46cde29aad47a1ac4a3f6c4cb32
Jean Trujillo <00zacks00@gmail.com>
2025-02-10 21:23:31 -0500

    refactor(records): Agrega comentarios y lista de por hacer

--- model/records/records_model.py
+++ model/records/records_model.py
@@ -1,19 +1,21 @@
 from librerias import *
 from model.generales.generales import *
-from uuid import uuid4
 from datetime import datetime
+
+# TODO: Separar lógica de agregar datos a cada tabla de la BD
+# TODO: Agregar validaciones de datos a cada tabla
+# TODO: Validar si el asesor existe en la tabla de asesores
 
 class recordsModel():
     def add_record(self):
         try:
-            # Generar UUID para el solicitante
-            asesores_info = supabase.table("ASESORES").select('*').eq('usuario', request.json.get('asesor_usuario')).execute()
+            agents_info = supabase.table("ASESORES").select('*').eq('usuario', request.json.get('asesor_usuario')).execute()
             print("Asesores info")
-            print(asesores_info.data)
-            asesor_id = asesores_info.data[0]['id']
+            print(agents_info.data)
+            agent_id = agents_info.data[0]['id']
 
             # Crear registro del solicitante
-            solicitante = {
+            applicant = {
                 "nombre_completo":  request.json.get('nombre_completo'),
                 "tipo_documento": request.json.get('tipo_documento'),
                 "numero_documento": request.json.get('numero_documento'),
@@ -26,15 +28,15 @@ class recordsModel():
                 "personas_a_cargo": request.json.get('personas_a_cargo')
             }
 
-            res = supabase.table('SOLICITANTES').insert(solicitante).execute()
+            res = supabase.table('SOLICITANTES').insert(applicant).execute()
             print("Solicitante")
             print(res.data)
 
-            solicitante_id = res.data[0]['id']
+            applicant_id = res.data[0]['id']
 
             # Crear registro de ubicación
-            ubicacion = {
-                "solicitante_id": solicitante_id,
+            location = {
+                "solicitante_id": applicant_id,
                 "direccion_residencia": request.json.get('direccion_residencia'),
                 "tipo_vivienda": request.json.get('tipo_vivienda'),
                 "barrio": request.json.get('barrio'),
@@ -43,14 +45,14 @@ class recordsModel():
                 "ciudad_gestion": request.json.get('ciudad_gestion')
             }
 
-            res = supabase.table('UBICACION').insert(ubicacion).execute()
+            res = supabase.table('UBICACION').insert(location).execute()
 
             print("Ubicaciones")
             print(res.data)
             
             # Crear registro de actividad económica
-            actividad_economica = {
-                "solicitante_id": solicitante_id,
+            economic_activity = {
+                "solicitante_id": applicant_id,
                 "actividad_economica": request.json.get('actividad_economica'),
                 "empresa_labora": request.json.get('empresa_labora'),
                 "fecha_vinculacion": datetime.strptime(request.json.get('fecha_vinculacion'), '%Y-%m-%d').strftime('%Y,%m,%d'),  # Comentado debido al error
@@ -60,14 +62,14 @@ class recordsModel():
                 "cargo_actual": request.json.get('cargo_actual')
             }
 
-            res = supabase.table('ACTIVIDAD_ECONOMICA').insert(actividad_economica).execute()
+            res = supabase.table('ACTIVIDAD_ECONOMICA').insert(economic_activity).execute()
 
             print("Actividad economica")
             print(res.data)
             
             # Crear registro de información financiera
-            info_financiera = {
-                "solicitante_id": solicitante_id,
+            financial_info = {
+                "solicitante_id": applicant_id,
                 "ingresos": request.json.get('ingresos'),
                 "valor_inmueble": request.json.get('valor_inmueble'),
                 "cuota_inicial": request.json.get('cuota_inicial'),
@@ -77,28 +79,28 @@ class recordsModel():
                 "total_pasivos": request.json.get('total_pasivos')
             }
 
-            res = supabase.table('INFORMACION_FINANCIERA').insert(info_financiera).execute()
+            res = supabase.table('INFORMACION_FINANCIERA').insert(financial_info).execute()
 
             print("Informacion financiera")
             print(res.data)
             
             # Crear registro de producto solicitado
-            producto = {
-                "solicitante_id": solicitante_id,
+            product = {
+                "solicitante_id": applicant_id,
                 "tipo_credito": request.json.get('tipo_credito'),
                 "plazo_meses": request.json.get('plazo_meses'),
                 "segundo_titular": request.json.get('segundo_titular'),
                 "observacion": request.json.get('observacion')
             }
 
-            res = supabase.table('PRODUCTO_SOLICITADO').insert(producto).execute()
+            res = supabase.table('PRODUCTO_SOLICITADO').insert(product).execute()
             print("Producto solicitado")
             print(res.data)
             
             # Crear registro de solicitud
             solicitud = {
-                "solicitante_id": solicitante_id,
-                "asesor_id": asesor_id,
+                "solicitante_id": applicant_id,
+                "asesor_id": agent_id,
                 "banco": request.json.get('banco'),
                 # "fecha_solicitud": datetime.now().strftime('%d/%m/%Y %H %M %S')
             }
@@ -107,11 +109,11 @@ class recordsModel():
             print("Solicitud")
             print(res.data)
 
-            print(solicitante)
-            print(ubicacion)
-            print(actividad_economica)
-            print(info_financiera)
-            print(producto)
+            print(applicant)
+            print(location)
+            print(economic_activity)
+            print(financial_info)
+            print(product)
             print(solicitud)
             
             return jsonify({
