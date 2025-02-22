b94f59fa9b0751f3ad896fcb3d235b9f8aea1f7e
Jean <00zacks00@gmail.com>
2025-02-22 09:07:38 -0500

    feat(login): fvvalidar si existe usuario y email correcto

---
+++ model/utils/login/validate_info.py
@@ -0,0 +1,16 @@
+import re
+from model.generales.generales import *
+
+def validar_email(email):
+    """Valida el formato del email usando una expresión regular"""
+    patron = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    return re.match(patron, email) is not None
+
+def usuario_existe(email):
+    """Verifica si un usuario ya existe en Supabase Auth"""
+    try:
+        response = supabase.auth.admin.list_users()
+        usuarios = response.data.users
+        return any(user.email == email for user in usuarios)
+    except Exception:
+        return False
