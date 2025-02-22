44a821af3b4d7b8c10e4200a567cc782140d25a9
Jean <00zacks00@gmail.com>
2025-02-22 09:18:46 -0500

    feat: Devuelve URL de una imagen por default

--- model/utils/login/fill_data.py
+++ model/utils/login/fill_data.py
@@ -1,8 +1,9 @@
 from model.generales.generales import *
 
-def get_business_image(self):
+def get_business_image():
     try:
-        response = supabase.storage.from_('business_images').download('default.jpg')
-        return response
+        # response = supabase.storage.from_('business_images').download('default.jpg')
+        default_image = "https://i.postimg.cc/RZJNTtyh/python.png"
+        return default_image
     except Exception as e:
         return jsonify({"msg": "Error al obtener la imagen de la empresa: " + str(e)}), 500
