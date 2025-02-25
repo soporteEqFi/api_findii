29298abc59f656784ce1f9832de03e04f83f1115
Jean <00zacks00@gmail.com>
2025-02-24 19:10:10 -0500

    feat: se agregan rutas de traer info de usuario y de agentes

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -4,6 +4,14 @@ mod_user = userModel()
 
 class userController():
 
-    def user_create(self):
+    def post_create_user(self):
         query = mod_user.create_user()
+        return query
+    
+    def get_agent_info(self, cedula):
+        query = mod_user.get_agent_info(cedula)
+        return query
+    
+    def get_user_info(self, cedula):
+        query = mod_user.get_user_info(cedula)
         return query
