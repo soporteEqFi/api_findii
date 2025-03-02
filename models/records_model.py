36f912e0b30e2c41118bbcacda87981f878aa2f4
Jean <00zacks00@gmail.com>
2025-03-02 11:51:34 -0500

    fix: se quita función e editar usuario

--- models/records_model.py
+++ models/records_model.py
@@ -501,31 +501,4 @@ class recordsModel():
             print("Ocurrió un error:", e)
             return jsonify({"error": "Ocurrió un error al procesar la solicitud"}), 500
         
-    def update_user(self):
-        try:
-            data_dict ={
-                "nombre": request.json.get('nombre'),
-                "cedula": request.json.get('cedula'),
-                "rol": request.json.get('rol'),
-                "empresa": request.json.get('empresa'),
-                
-            }
-
-            id = request.json.get('id')
-            # cedula = request.json.get('cedula_usuario')
-
-            campos_vacios = diccionario_vacio(data_dict)
-
-            if campos_vacios:
-                return jsonify({"registrar_agente_status": "existen campos vacios", "campos_vacios": campos_vacios}), 400      
-
-            supabase.table('TABLA_USUARIOS').update(data_dict).eq('id', id).execute()
-            
-
-            return jsonify({"actualizar_agente": "OK"}), 200
-        
-        except Exception as e:
-            print("Ocurrió un error:", e)
-            return jsonify({"mensaje": "Ocurrió un error al procesar la solicitud."}), 500
-        
     
