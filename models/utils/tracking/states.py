8a94b03a0e718d8dd923bd79fcc10b94ed030017
Jean <00zacks00@gmail.com>
2025-04-27 12:49:48 -0500

    feat: archivo para traer los nombres de los posibles estados

---
+++ models/utils/tracking/states.py
@@ -0,0 +1,12 @@
+def get_state_avaiable():
+    return {
+        "pendiente": "Pendiente",
+        "en_proceso": "En proceso",
+        "completado": "Completado",
+        "rechazado": "Rechazado",
+        "cancelado": "Cancelado",
+        "en_revision": "En revisión",
+        "corregir": "Corregir datos",
+        "corregir_documentos": "Corregir documentos",
+        "error": "Error",
+    }
