c630746388142e4eb92276719f96b1cd484041a8
Jean <00zacks00@gmail.com>
2025-08-17 10:09:38 -0500

    version mejorada

--- controllers/controlador_imagen.py
+++
@@ -1,9 +0,0 @@
-from models.insertar_imagen import *
-
-modimagen = insertar()
-
-class insertar_imagen_controlador():
-
-    def insertar_imagen(self):
-        query = modimagen.insertar_imagen()
-        return query
--- models/utils/login/fill_data.py
+++
@@ -1,9 +0,0 @@
-from models.generales.generales import *
-
-def get_business_image():
-    try:
-        # response = supabase.storage.from_('business_images').download('default.jpg')
-        default_image = "https://i.postimg.cc/RZJNTtyh/python.png"
-        return default_image
-    except Exception as e:
-        return jsonify({"msg": "Error al obtener la imagen de la empresa: " + str(e)}), 500
